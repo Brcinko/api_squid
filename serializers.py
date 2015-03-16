21343b383794bdf8a3ddffb375b9652e441e68de
Brcinko <lukas.klescinec@gmail.com>
2015-03-16 13:42:58 +0100

    Added two Auth serializers

--- serializers.py
+++ serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api_squid.models import AclRule, AclList
+from api_squid.models import AclRule, AclList, Authentication, AuthenticationDB
 
 
 class AclRuleSerializer(serializers.ModelSerializer):
@@ -14,3 +14,17 @@ class AclListSerializer(serializers.ModelSerializer):
     class Meta:
         model = AclList
         fields = ('id', 'acl_rules', 'deny_value', 'list_type')
+
+
+class AuthenticationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Authentication
+        fields = ('id', 'realm', 'children', 'program', 'case_sensitive', 'credentialsttl', 'utf8')
+
+
+class AuthenticationDBSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AuthenticationDB
+        fields = ('id', 'username_column', 'password_column', 'database_name', 'user', 'password', 'table', 'encryption')
