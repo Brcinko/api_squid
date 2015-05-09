4bff4b7cab4ecce2043e7bdea3c7d47fe6a1097b
Lukáš Kleščinec <lukas.klescinec@gmail.com>
2015-05-09 21:09:40 +0200

    New comments in views for OPTIONS

--- views.py
+++ views.py
@@ -14,20 +14,26 @@ from settings import *
 
 
 def index(request):
-    return HttpResponse("This is a index web page of squid API.")
+    return Response("This is a index web page of squid API.")
 
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def acl_rules_list(request):
     """
-    List of all acl-rules, or create a new acl-rule.
-    acl_values have to be in this json form:
-        {"values":[
-           "192.168.0.0/24",
-           "127.0.0.0/24"
-        ]
-        }
+    Get list of all acl-rules, or create a new acl-rule.
+    acl rule contains: 
+        acl_name - CharField
+        acl_type - CharField - just one of these acl-elements: http://wiki.squid-cache.org/SquidFaq/SquidAcl#ACL_elements 
+        acl_values - JSON - have to be in this json form:
+            {"values":[
+               "192.168.0.0/24",
+               "127.0.0.0/24"
+            ]
+            }
+    Allowed method:
+        GET  - response to this method return rest_framework Response with list of all serialized acl rules stored in DB
+        POST - response to this method save new acl rule into db and return rest_framework Response with serialized record
     """
     if request.method == 'GET':
         rules = AclRule.objects.all()
@@ -48,6 +54,14 @@ def acl_rules_list(request):
 def acl_rule_detail(request, pk):
     """
     Detail of specific acl-rule
+    Allowed methods:
+        GET    - response to this method return rest_framework Response with serialized data of specific acl-rule
+        DELETE - response delete specific acl-rule from DB and return rest_framewor Response with code 204
+        PUT    - response to this method change specific acl-rule in DB and 
+                 return rest_framewrok Response with serialized data of specific acl-rule
+    Params: request - requested data
+            pk - ID of specific record
+    For more info about requested data try OPTIONS on yourdomain.com/api_squid/acl_rules/
     """
     try:
         rule = AclRule.objects.get(pk=pk)
@@ -75,7 +89,16 @@ def acl_rule_detail(request, pk):
 @api_view(['GET', 'POST'])
 def acl_list(request):
     """
-    List all acl-lists, or create a new acl-list.
+    Get list all acl-lists, or create a new acl-list.
+    acl-list contains: 
+        id         - Integer (PK)
+        acl_rule   - acl_rule obj
+        deny_value - Boolean 
+        list_type  - CharField - only "http_acces" is allowed
+    Allowed method:
+        GET  - response to this method return rest_framework Response with list of all serialized acl-lists stored in DB
+        POST - response to this method save new acl rule into db and return rest_framework Response with serialized record
+    Params: request - requested data
     """
     if request.method == 'GET':
         acl_lists = AclList.objects.all()
@@ -96,6 +119,14 @@ def acl_list(request):
 def acl_list_detail(request, pk):
     """
     Detailed view of one access pattern
+    Allowed methods:
+        GET    - response to this method return rest_framework Response with serialized data of specific acl-list
+        DELETE - response delete specific acl-list from DB and return rest_framework Response with code 204
+        PUT    - response to this method change specific acl-list in DB and 
+                 return rest_framewrok Response with serialized data of specific acl-list
+    Params: request - requested data
+            pk - ID of specific record
+    For more info about requested data try OPTIONS on yourdomain.com/api_squid/acl_list/
     """
     try:
         pattern = AclList.objects.get(pk=pk)
@@ -123,7 +154,21 @@ def acl_list_detail(request, pk):
 @api_view(['GET', 'POST'])
 def authentication(request):
     """
-    List of all authentication settings
+    Get list of all authentication settings, or create a new one
+    authentication contains: 
+        realm             - CharField
+        children          - Integer
+        enabled           - Boolean 
+        program           - Charfield 
+        case_sensitive    - Boolean
+        credentialsttl    - Integer
+        utf8              - Boolean
+        authentication_db - authentication_db obj
+        Explanation of parameters: http://www.squid-cache.org/Doc/config/auth_param/
+    Allowed method:
+        GET  - response to this method return rest_framework Response with list of all serialized acl-lists stored in DB
+        POST - response to this method save new acl rule into db and return rest_framework Response with serialized record
+    Params: request - requested data
     """
     if request.method == 'GET':
         auth = Authentication.objects.all()
@@ -143,7 +188,16 @@ def authentication(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def authentication_detail(request, pk):
     """
-    Detailed view of one access pattern
+    Detailed view of one authentication parame record
+    Allowed methods:
+        GET    - response to this method return rest_framewrok Response with serialized data of specific auth params
+        DELETE - response delete specific auth params and return rest_framewor Response with code 204
+        PUT    - response to this method change specific auth params in DB and 
+                 return rest_framewrok Response with serialized data of specific auth params
+                 
+    Params: request - requested data
+            pk - ID of specific record
+    For more info about requested data try OPTIONS on yourdomain.com/api_squid/authentication/
     """
     try:
         auth = Authentication.objects.get(pk=pk)
@@ -172,6 +226,20 @@ def authentication_detail(request, pk):
 def authentication_db(request):
     """
     List of all database settings for authentication
+    authenticationDB contains: 
+        username_column - CharField
+        password_column - CharField
+        database_name   - CharField 
+        user            - Charfield 
+        password        - Charfield
+        credentialsttl  - Charfield
+        table           - Charfield
+        encryption      - Charfield - only "md5" or "plaintext"
+        Explanation of parameters: http://www.squid-cache.org/Versions/v3/3.2/manuals/basic_db_auth.html
+    Allowed method:
+        GET  - response to this method return rest_framework Response with list of all serialized acl-lists stored in DB
+        POST - response to this method save new acl rule into db and return rest_framework Response with serialized record
+    Params: request - requested data
     """
     if request.method == 'GET':
         db = AuthenticationDB.objects.all()
@@ -190,6 +258,15 @@ def authentication_db(request):
 def authentication_db_detail(request, pk):
     """
     Detailed view of specific record of authentification database settings
+    Allowed methods:
+        GET    - response to this method return rest_framewrok Response with serialized data of specific user DB
+        DELETE - response delete specific user DB record and return rest_framewor Response with code 204
+        PUT    - response to this method change specific user DB record in DB and 
+                 return rest_framewrok Response with serialized data of specific user DB
+    
+    Params: request - requested data
+            pk - ID of specific record
+    For more info about requested data try OPTIONS on yourdomain.com/api_squid/authentication_db/
     """
     try:
         db = AuthenticationDB.objects.get(pk=pk)
@@ -216,6 +293,13 @@ def authentication_db_detail(request, pk):
 @csrf_exempt
 @api_view(['GET', 'PUT'])
 def update_config(request):
+    """
+    Generating squid.conf file from database
+    Allowed methods:
+        GET    - response to this method generate new config file of Squid proxy and ad new version of acl-lists into DB
+    
+    Params: request - requested data
+    """
     if request.method == 'GET':  # TODO method will be PUT?
         auth = Authentication.objects.latest('id')
         rules = AclRule.objects.all()
